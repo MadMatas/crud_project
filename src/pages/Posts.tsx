925b17483ae20d960069d6e93b0c15c1c4a12314
Moksleivis <Moksleivis@kitm.local>
2026-05-18 11:02:31 +0300

    Minor changes

--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -2,6 +2,23 @@ import React from "react";
 import { Button } from "@/components/ui/button";
 
 export function Posts() {
+   
+    async function getPosts() {
+  const url = "/api/posts";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+    const posts = getPosts();
+    getPosts();
     return (
         <div className="max-w-md mx-auto p-4">
             <div className="bg-slate-200">
@@ -11,15 +28,15 @@ export function Posts() {
                     <div className="max-w-5xl mx-auto p-6">
                         <div className="bg-white rounded-xl shadow overflow-hidden">
 
-                            <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
-                                {post.image.map((image_url, i) => (
+                            {/* <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+                                {post.image_url.map((image_url, i) => (
                                     <img
                                         key={i}
                                         src={image_url}
                                         className="w-full h-64 object-cover"
                                     />
                                 ))}
-                            </div>
+                            </div> */}
                         </div>
                     </div>
                 </div>
